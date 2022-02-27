eba09a6a867348d153286a528764bc891743ffec
Susanta <inbox.susanta@gmail.com>
2022-02-27 17:14:55 +0530

    Added next-theme package for dark mode

--- components/Header.js
+++ components/Header.js
@@ -8,13 +8,31 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import AppsOutlinedIcon from '@mui/icons-material/AppsOutlined';
 import { Avatar } from '@mui/material';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 function Header() {
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, resolvedTheme, theme } = useTheme();
+
+  // After mounting, we have access to the theme
+  useEffect(() => setMounted(true), []);
+
+  //   console.log('Current Theme : ', theme);
+
   return (
-    <header>
+    <header className="sticky top-0 z-40 bg-white dark:bg-[#1D2226] flex items-center justify-around py-1.5 px-3 focus-within:shadow-lg">
       {/* Left */}
       <div className="flex items-center space-x-2 w-full max-w-xs">
-        <Image src="https://rb.gy/bizvqj" width={45} height={45} />
+        {mounted && (
+          <>
+            {resolvedTheme === 'dark' ? (
+              <Image src="https://rb.gy/bizvqj" width={45} height={45} />
+            ) : (
+              <Image src="https://rb.gy/dpmd9s" width={55} height={55} />
+            )}
+          </>
+        )}
 
         <div className="flex items-center space-x-1 dark:md:bg-gray-700 py-2.5 px-4 rounded w-full">
           <SearchRoundedIcon />
