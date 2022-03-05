d044e75782de695a504dfa3b787f479568657caa
Susanta <inbox.susanta@gmail.com>
2022-03-05 19:50:15 +0530

    Fetch all posts from front-end

--- components/Feed.js
+++ components/Feed.js
@@ -1,6 +1,28 @@
+import { useEffect, useState } from 'react';
+
 import Input from './Input';
 
 function Feed() {
+  const [realTimePosts, setRealTimePosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('api/posts', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const responseData = await response.json();
+      setRealTimePosts(responseData);
+    };
+
+    fetchPosts();
+  }, []);
+
+  console.log(realTimePosts);
+
   return (
     <div className="space-y-6 pb-24 mx-w-lg">
       <Input />
