23b7092f90fa00a4a594dfe26eef44267e27e86d
Susanta <inbox.susanta@gmail.com>
2022-03-05 10:59:05 +0530

    Feed post input

---
+++ components/Feed.js
@@ -0,0 +1,12 @@
+import Input from './Input';
+
+function Feed() {
+  return (
+    <div className="space-y-6 pb-24 mx-w-lg">
+      <Input />
+      {/* Posts */}
+    </div>
+  );
+}
+
+export default Feed;
